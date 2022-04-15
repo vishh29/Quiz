832562d6987aed5ca61aff976a1f50fa646a8d47
vishnu nayak <vishnunayak70@gmail.com>
2022-04-15 23:36:19 +0530

    url update

--- app.js
+++ app.js
@@ -12,54 +12,57 @@ const queDiv = document.getElementById("question-div");
 
 const formDiv = document.getElementById("form-div");
 
-
 let currentQue = 0;
 
 function getFormData() {
-    var Url;
-    if (!amount.value) {
-        console.log('error hai');
-    } else if (amount.value) {
-        Url = `https://opentdb.com/api.php?amount=${amount.value}`;
-    } else if (category.value && category.value != 'any') {
-        Url = `https://opentdb.com/api.php?amount=${amount.value}&category=${category.value}`
-    } else if (difficulty.value && difficulty.value != 'any') {
-        Url = `https://opentdb.com/api.php?amount=${amount.value}&category=${category.value}&difficulty=${difficulty.value}`
-    } else if (type.value && type.value != 'any') {
-        Url = `https://opentdb.com/api.php?amount=${amount.value}&category=${category.value}&difficulty=${difficulty.value}&type=${type.value}`;
-    }
+  var Url;
+  if (!amount.value) {
+    console.log("error hai");
+  } else {
+    Url = `https://opentdb.com/api.php?amount=${amount.value}`;
+  }
+  if (category.value && category.value != "any") {
+    Url = Url.concat("", `&category=${category.value}`);
+  }
+  if (difficulty.value && difficulty.value != "any") {
+    Url = Url.concat("", `&difficulty=${difficulty.value}`);
+  }
+  if (type.value && type.value != "any") {
+    Url = Url.concat("", `&type=${type.value}`);
+  }
 
-    fetchUrl(Url)
+  fetchUrl(Url);
 }
 
 async function fetchUrl(url) {
   const response = await fetch(url);
   const data = await response.json();
-    // console.log(data);
-    setQue(data.results)
+  // console.log(data);
+  setQue(data.results);
 }
 
 function setQue(arr) {
-    console.log(arr);
-    var que = arr[currentQue]
-    console.log(que);
-    q.textContent = que.question
+  console.log(arr);
+  var que = arr[currentQue];
+  console.log(que);
+  q.textContent = que.question;
 
-    var options = que.incorrect_answers
-    options.splice(Math.floor(Math.random() * options.length+1),0,que.correct_answer)
-    setOptions(options)
+  var options = que.incorrect_answers;
+  options.splice(
+    Math.floor(Math.random() * options.length + 1),
+    0,
+    que.correct_answer
+  );
+  setOptions(options);
 }
 
 function setOptions(opts) {
-    option.forEach((e,i) => {
-        e.value = opts[i]
-    })
+  option.forEach((e, i) => {
+    e.value = opts[i];
+  });
 
-    optionLabel.forEach((e, i) => {
-      e.textContent = opts[i];
-    });
-    formDiv.style.display = 'none'
+  optionLabel.forEach((e, i) => {
+    e.textContent = opts[i];
+  });
+  formDiv.style.display = "none";
 }
-
-
-
